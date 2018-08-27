01c67046e15cedc86dd512e6583b67cbe7a139b5
sergio.torres.lozano <sergio.torres.lozano@accenture.com>
2018-08-27 22:09:44 +0300

    Added Controller tests and Actuator tests

--- src/test/java/com/chuck/service/ChuckServiceApplicationTests.java
+++ src/test/java/com/chuck/service/ChuckServiceApplicationTests.java
@@ -1,16 +1,46 @@
 package com.chuck.service;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = { "management.port=0" })
 public class ChuckServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Value("${local.management.port}")
+	private int mgt;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate
+				.getForEntity("http://localhost:" + this.mgt + "/actuator/info", Map.class);
+		assertEquals(entity.getStatusCode(), HttpStatus.OK);
+//		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
