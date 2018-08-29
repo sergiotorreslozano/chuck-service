cac662c6cdcade452c732154802bb42532ecd381
sergio.torres.lozano <sergio.torres.lozano@accenture.com>
2018-08-29 10:15:37 +0300

    Cosmetic changes

--- src/main/java/com/chuck/service/ChuckController.java
+++ src/main/java/com/chuck/service/ChuckController.java
@@ -1,11 +1,8 @@
 package com.chuck.service;
 
-import java.lang.invoke.MethodHandles;
 import java.util.Optional;
 import java.util.Random;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class ChuckController {
 
-	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
 	private ChuckRepository repository;
+
+	private Random r = new Random();
 
 	/**
 	 * @param repository
@@ -27,8 +24,6 @@ class ChuckController {
 
 	@RequestMapping("/chuck")
 	public @ResponseBody Optional<ChuckFact> greeting() {
-		log.debug("Got a request!");
-		Random r = new Random();
 		return repository.findById(r.nextInt(10));
 	}
 	
