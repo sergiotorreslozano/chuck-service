d59a63f27b3936af1ec615ce77102d02e4d582bd
storreslozano <sergio.torres.lozano@accenture.com>
2020-06-01 15:56:42 +0200

    Added more Chuck jokes

--- src/main/java/com/chuck/service/ChuckLoader.java
+++ src/main/java/com/chuck/service/ChuckLoader.java
@@ -46,6 +46,15 @@ public class ChuckLoader {
 				"When Chuck Norris swims in the ocean, the sharks are in a steel cage"));
 		repository.save(new ChuckFact(0,
 				"Chuck Norris will never have a heart attack. His heart isn't nearly foolish enough to attack him"));
+		repository.save(new ChuckFact(10, "Chuck Norris counted to infinity… twice"));
+		repository.save(new ChuckFact(12, "Chuck Norris can speak Braille"));
+		repository.save(new ChuckFact(13, "Chuck Norris once won a game of Connect Four in three moves"));
+		repository.save(new ChuckFact(14, "The dark is afraid of Chuck Norris"));
+		repository.save(new ChuckFact(15, "Chuck Norris can kill two stones with one bird"));
+		repository.save(new ChuckFact(16, "Chuck Norris can play the violin with a piano"));
+		repository.save(new ChuckFact(17, "Chuck Norris can build a snowman out of rain"));
+		repository.save(new ChuckFact(18, "When Chuck Norris enters a room, he doesn’t turn the lights on, he turns the dark off"));
+		repository.save(new ChuckFact(19, "Chuck Norris can tie his shoes with his feet"));
 	}
 	
 	
